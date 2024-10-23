ee1227053ddd2d66bd19e30045963ec008378d52
unknown <alexwood319@gmail.com>
2024-10-23 12:16:36 -0700

    Polished up heat collection graybox

--- Assets/Scripts/CollectableSprite.cs
+++ Assets/Scripts/CollectableSprite.cs
@@ -9,24 +9,44 @@ public class CollectableSprite : MonoBehaviour
     // Must be assigned in the inspector
     [SerializeField] RectTransform rt;
     [SerializeField] Button button;
-    [SerializeField] float speed = 5f;
+    [SerializeField] Renderer buttonRend;
+    [SerializeField] float minSpeed = 200f;
+    [SerializeField] float maxSpeed = 400f;
+    float speed;
 
     public Vector2 direction = Vector2.zero;
 
     void Awake()
     {
         button.onClick.AddListener(OnButtonClicked);
+        speed = Random.Range(minSpeed, maxSpeed);
+    }
+
+    void OnDestroy()
+    {
+        HeatCollectionManager.RemoveSprite(this);
     }
 
     void Update()
     {
         rt.Translate(direction * speed * Time.deltaTime);
+        if (IsOffScreen())
+        {
+            Destroy(gameObject);
+        }
     }
 
     void OnButtonClicked()
     {
         SpriteCounter.CollectSprite();
-        HeatCollectionManager.RemoveSprite(this);
         Destroy(gameObject);
+    }
+
+    bool IsOffScreen()
+    {
+        return rt.anchoredPosition.x >= (Screen.width + rt.sizeDelta.x) / 2f ||
+               rt.anchoredPosition.x <= (-Screen.width - rt.sizeDelta.x) / 2f ||
+               rt.anchoredPosition.y >= (Screen.height + rt.sizeDelta.y) / 2f ||
+               rt.anchoredPosition.y <= (-Screen.height - rt.sizeDelta.y) / 2f;
     }
 }
